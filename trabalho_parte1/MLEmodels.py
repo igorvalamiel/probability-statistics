a704488b1cf5b4cc7cf5c54a9f58cc82f4586532
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2025-10-25 12:11:31 -0300

    função gamma completa, falta QQ plot

--- trabalho_parte1/MLEmodels.py
+++ trabalho_parte1/MLEmodels.py
@@ -44,15 +44,15 @@ ser07pl = clean(ser07pl)
 # =========================================================================================================================================
 # Calculing Gamma MLE
 
-def MLE_Gamma(data, graphSize=(12,6)):
+def MLE_Gamma(data, xname, title, graphSize=(12,6)):
     # getting k and theta
     gammaData = np.array(data)
-    k_mle, loc_mle, theta_mle = stats.gamma.fit(gammaData)
-    print(f"Parâmetros MLE estimados: k = {k_mle:.4f}, θ = {theta_mle:.4f}\n")
+    k_mle, loc_mle, theta_mle = stats.gamma.fit(gammaData, floc=0)
+    print(f"Parâmetros MLE estimados: k = {k_mle}, loc = {loc_mle} , θ = {theta_mle}\n")
     
     # Defining "borders" -> Gamma is defined fo x > 0
     x_min = max(0, gammaData.min()) # We have to make this to ajust the inferior border
-    x_max = gammaData.max() # We have to make this to ajust the superior border
+    x_max = gammaData.max()# We have to make this to ajust the superior border
     x_grid = np.linspace(x_min, x_max, 1000)
 
     # Creating the PDF
@@ -62,22 +62,22 @@ def MLE_Gamma(data, graphSize=(12,6)):
     plt.figure(figsize=graphSize)
 
     #creating the histogram
-    plt.hist(gammaData, bins=40, density=True, alpha=0.7, color='#00B30E', label='Dados Reais (Float)', edgecolor='black', zorder=1)
+    plt.hist(gammaData, bins=40, density=True, alpha=0.7, color='#00B30E', label='PDF['+xname+']', edgecolor='black', zorder=1)
 
     #creating the gamma function
     plt.plot(x_grid, pdf_gamma, '#E88300', linewidth=2.5, label=f'Gamma Ajustada (k={k_mle:.3f}, θ={theta_mle:.3f})', zorder=2)
 
     #creating the graph
-    plt.xlabel('Valor', fontsize=12)
-    plt.ylabel('Densidade de Probabilidade', fontsize=12)
-    plt.title('Comparação: Dados Float vs Distribuição Gamma Ajustada (MLE)', fontsize=14)
+    plt.xlabel(xname, fontsize=12)
+    plt.title(title, fontsize=14)
     plt.legend(fontsize=11)
     plt.grid(alpha=0.3, zorder=0)
     plt.tight_layout()
-    #plt.yscale('log')
+    plt.yscale('log')
 
     plt.show()
 
-
-#MLE_Gamma(cli11dt)
-MLE_Gamma(ser07dt)
+#MLE_Gamma(cli11dt, "Download Throughput", "Cliente 11 - Download Throughput - Distribuição Gamma", (14,7))
+#MLE_Gamma(ser07dt, "Download Throughput", "Servidor 07 - Download Throughput - Distribuição Gamma", (14,7))
+MLE_Gamma(cli11ut, "Upload Throughput", "Cliente 11 - Upload Throughput - Distribuição Gamma", (14,7))
+MLE_Gamma(ser07ut, "Upload Throughput", "Servidor 07 - Upload Throughput - Distribuição Gamma", (14,7))
